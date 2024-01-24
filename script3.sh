10c05a258ca39011d1fb827ffe5c1b2aacff4c12
JD <jaideephec1986@gmail.com>
2024-01-24 23:12:50 +0530

    this is script to read value

---
+++ script3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#THis shell script is to read value from console
+
+echo " Enter the value of a"
+
+read a
+
+echo " Enter the value of b "
+
+read b
+
+
+echo " This is value of $a and this is value of $b"
+
+echo " welcome to git bash"
+
